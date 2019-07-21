fd90ddab1ada6a67aff3f1843c5e46bc0eae5258
Ashok Dhaduk <ashok.r.dhaduk@gmail.com>
2019-07-21 18:21:47 +0530

    First Commit: Intial Version 1.0.0

---
+++ process/js/app.js
@@ -0,0 +1,37 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var MainInterface = React.createClass({
+  getInitialState: function(){
+    return {
+        title: 'Appointments',
+        show: true
+    }  // return
+  }, // getInitialState
+  render: function() {
+    var showTitle = '';
+    if( this.state.show ) {
+        showTitle = "New";
+    }
+    var displayList = {
+        display: this.state.show ? 'block' : 'none',
+        color: 'red',
+        //fontSize: '50px'
+    }
+    return (
+      <div className="interface">
+        <h1>{ showTitle } { this.state.title }</h1>
+        <ul style={displayList}>
+          <li>Buffy 3:30 PM</li>
+          <li>Spot 8:30 PM</li>
+          <li>Goldie 3:50 PM</li>
+        </ul>
+      </div>
+    )
+  } //render
+}); //MainInterface
+
+ReactDOM.render(
+  <MainInterface />,
+  document.getElementById('petAppointments')
+); //render
